a58f3c66a2cf09e4487ae241b6185975f3d29e69
iamaomms <pimnipa.wiw@dome.tu.ac.th>
2025-02-18 12:19:30 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -31,37 +30,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
-=======
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const productRoutes = require('./routes/productRoutes');
-require('dotenv').config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use('/api', productRoutes);
-
-// เชื่อมต่อ MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('✅ MongoDB Connected'))
-  .catch(err => console.log('❌ MongoDB Error:', err));
-
-// เส้นทางทดสอบ API
-app.get('/', (req, res) => {
-    res.send('🎉 Welcome to REST API!');
-});
-
-// เริ่มต้นเซิร์ฟเวอร์
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
->>>>>>> f27c38f2f1a14951023deab46bdc7b10a0585a70
